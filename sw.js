6ee422d2d2dbe3a76f9182ea9933de052754f2d5
l3077l <bagirovisa2008@gmail.com>
2026-05-18 14:27:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,54 @@
-const CACHE_NAME = 'lcb-pda-v2';
-const ASSETS = [
-  './',
-  'index.html',
-  'manifest.json',
-  '01. In Hell We Live, Lament.mp3',
-  '04. Dongbaek.mp3',
-  '01. Dungeon Theme.mp3'
+const playlist = [
+    "01. In Hell We Live, Lament.mp3",
+    "04. Dongbaek.mp3",
+    "01. Dungeon Theme.mp3"
 ];
+let currentTrackIndex = 0;
+const audio = document.getElementById('bgMusic');
+const statusText = document.getElementById('musicStatus');
+const playBtn = document.getElementById('playBtn');
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
+function loadTrack(index) {
+    audio.src = playlist[index];
+    audio.play().then(() => updateUI(true)).catch(showError);
+}
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
+function toggleMusic() {
+    if (!audio.src || audio.src === "") {
+        loadTrack(currentTrackIndex);
+    } else if (audio.paused) {
+        audio.play().then(() => updateUI(true)).catch(showError);
+    } else {
+        audio.pause();
+        updateUI(false);
+    }
+}
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
-  );
-});
+function nextTrack() {
+    currentTrackIndex = (currentTrackIndex + 1) % playlist.length;
+    loadTrack(currentTrackIndex);
+}
+
+function prevTrack() {
+    currentTrackIndex = (currentTrackIndex - 1 + playlist.length) % playlist.length;
+    loadTrack(currentTrackIndex);
+}
+
+function updateUI(isPlaying) {
+    if (isPlaying) {
+        statusText.innerText = "TRACK " + (currentTrackIndex + 1);
+        statusText.style.color = "#4ade80";
+        playBtn.innerText = "PAUSE";
+    } else {
+        statusText.innerText = "НА ПАУЗЕ";
+        statusText.style.color = "#6b7280";
+        playBtn.innerText = "PLAY";
+    }
+}
+
+function showError() {
+    statusText.innerText = "ОШИБКА";
+    statusText.style.color = "#bf1919";
+}
+
+audio.onended = nextTrack; // Автопереключение
